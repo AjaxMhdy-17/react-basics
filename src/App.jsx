7f7dc4b7680ba696163e5e97139a970560753b9d
MahedyHHasan17 <mhdyhasan017@gmail.com>
2025-06-12 12:59:55 +0600

    state

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,45 @@
 import { useState } from "react";
 import "../src/App.css";
 
-const messages = [
-  "Learn React ⚛️",
-  "Apply for jobs 💼",
-  "Invest your new income 🤑",
-];
-
 function App() {
-  const [step, setStep] = useState(1);
-  const [test, setTest] = useState({ name: "Jonas" });
-  const [isOpen, setIsOpen] = useState(true);
+  const [step, setStep] = useState(0);
+  const [count, setCount] = useState(0);
 
-  const next = () => {
-    if (step < 3) setStep((step) => step + 1);
-    setTest({ name: "Hasan" });
+  const date = new Date("jun 17 2027");
+  date.setDate(date.getDate() + count);
+
+  const countInc = () => {
+    setCount((count) => count + step);
   };
 
-  const prev = () => {
-    if (step > 1) setStep((step) => step - 1);
-    setTest({ name: "Jonas" });
+  const countDec = () => {
+    setCount((count) => count - step);
   };
 
   return (
-    <>
-      <button className="close" onClick={() => setIsOpen(!isOpen)}>
-        &times;
-      </button>
-      {isOpen && (
-        <div className="steps">
-          <div className="numbers">
-            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
-          </div>
+    <main>
+      <div>
+        <button onClick={() => setStep((step) => step - 1)}>-</button>
+        Step : {step}
+        <button onClick={() => setStep((step) => step + 1)}>+</button>
+      </div>
+      <div>
+        <button onClick={countDec}>-</button>
+        Count : {count}
+        <button onClick={countInc}>+</button>
+      </div>
 
-          <p className="message">
-            {messages[step - 1]} {test.name}
-          </p>
-
-          <div className="buttons">
-            <button
-              onClick={prev}
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-            >
-              Previous
-            </button>
-            <button
-              onClick={next}
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-            >
-              Next
-            </button>
-          </div>
-        </div>
-      )}
-    </>
+      <div>
+        <span>
+          {count == 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </div>
+    </main>
   );
 }
 

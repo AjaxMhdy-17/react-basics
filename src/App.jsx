f2ad37c99105ea5f349caebaa728106e2a565b08
MahedyHHasan17 <mhdyhasan017@gmail.com>
2025-06-13 10:18:55 +0600

    adding components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
 import "../src/App.css";
+import Logo from "./components/Logo";
+import Form from "./components/Form";
+import PackingList from "./components/PackingList";
+import Stats from "./components/Stats";
 
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
@@ -45,134 +49,6 @@ function App() {
       />
       <Stats items={items} />
     </main>
-  );
-}
-
-function Logo() {
-  return <h1>Far Away</h1>;
-}
-function Form({ handleAddItems }) {
-  const [quantity, setQuantity] = useState("");
-  const [description, setDescription] = useState("");
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (!description) return;
-    const newItem = {
-      id: Date.now(),
-      description: description,
-      quantity: quantity,
-      packed: false,
-    };
-    handleAddItems(newItem);
-    setQuantity("");
-    setDescription("");
-  };
-
-  return (
-    <form className="add-form" onSubmit={handleSubmit}>
-      <h3>What do you need for your trip ?</h3>
-      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
-        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-          <option value={num} key={num}>
-            {num}
-          </option>
-        ))}
-      </select>
-      <input
-        type="text"
-        placeholder="Item..."
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-      />
-      <button>Add</button>
-    </form>
-  );
-}
-
-function PackingList({ items, handleDelete, handleCheck, handleClearItem }) {
-  const [sortBy, setSortBy] = useState("input");
-
-  let sortedItems;
-
-  if (sortBy == "input") sortedItems = items;
-  if (sortBy == "description")
-    sortedItems = items
-      .slice()
-      .sort((a, b) => a.description.localeCompare(b.description));
-
-  if (sortBy == "packed")
-    sortedItems = items
-      .slice()
-      .sort((a, b) => Number(a.packed) - Number(b.packed));
-
-  return (
-    <div className="list">
-      <ul>
-        {sortedItems.map((item) => (
-          <Item
-            key={item.id}
-            item={item}
-            handleDelete={handleDelete}
-            handleCheck={handleCheck}
-          />
-        ))}
-      </ul>
-
-      <div className="actions">
-        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
-          <option value="input">Sort by input order</option>
-          <option value="description">Sort by description</option>
-          <option value="packed">Sort by packed status</option>
-        </select>
-        <button onClick={handleClearItem}>clear list</button>
-      </div>
-      <div></div>
-    </div>
-  );
-}
-
-function Item({ item, handleDelete, handleCheck }) {
-  const line = { textDecoration: "line-through" };
-  return (
-    <li>
-      {/* <span style={item.packed == true ?  line : {}}>{item.quantity} {item.description}</span> */}
-      <input
-        type="checkbox"
-        value={item.packed}
-        onClick={() => handleCheck(item.id)}
-      />
-      <span className={item.packed == true ? "line" : ""}>
-        {item.quantity} {item.description}
-      </span>
-      <button onClick={() => handleDelete(item.id)}>x</button>
-    </li>
-  );
-}
-
-function Stats({ items }) {
-  if (!items.length) {
-    return (
-      <p className="stats">
-        <em>Start adding some items to your packing list</em>
-      </p>
-    );
-  }
-
-  const list = items.length;
-  const packed = items.filter((item) => item.packed == true).length;
-  const percentage = Math.round((packed / list) * 100);
-
-  return (
-    <footer className="stats">
-      <em>
-        {percentage === 100
-          ? "You have got everything to go."
-          : `
-          You have ${list} items on your list, and you already packed (${percentage}%)
-        `}
-      </em>
-    </footer>
   );
 }
 
---
+++ src/components/Logo.jsx
@@ -0,0 +1,6 @@
+
+const Logo = () => {
+   return <h1>Far Away</h1>;
+}
+
+export default Logo

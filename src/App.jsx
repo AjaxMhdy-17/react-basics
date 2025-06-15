2279167cd4accf2ec0745864f7db3ab4c47a1393
MahedyHHasan17 <mhdyhasan017@gmail.com>
2025-06-15 13:09:47 +0600

    usePopCorn spliting up into components

--- src/App.jsx
+++ src/App.jsx
@@ -1,94 +1,228 @@
 import { useState } from "react";
-import "../src/App.css";
+import "./App.css";
 
-function App() {
+const tempMovieData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt0133093",
+    Title: "The Matrix",
+    Year: "1999",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt6751668",
+    Title: "Parasite",
+    Year: "2019",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+  },
+];
+
+const tempWatchedData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    runtime: 148,
+    imdbRating: 8.8,
+    userRating: 10,
+  },
+  {
+    imdbID: "tt0088763",
+    Title: "Back to the Future",
+    Year: "1985",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+    runtime: 116,
+    imdbRating: 8.5,
+    userRating: 9,
+  },
+];
+
+const average = (arr) =>
+  arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+
+export default function App() {
   return (
-    <main style={{ textAlign: "center", marginTop: "30px" }}>
-      <TipCalculator />
-    </main>
+    <>
+      <Navbar />
+      <Main />
+    </>
   );
 }
 
-const TipCalculator = () => {
-  const [bill, setBill] = useState(0);
-  const [percentage1, setPercentage1] = useState(0);
-  const [percentage2, setPercentage2] = useState(0);
+const Navbar = () => {
+  return (
+    <nav className="nav-bar">
+      <Logo />
+      <Search />
+      <p className="num-results">
+        Found <strong>X</strong> results
+      </p>
+    </nav>
+  );
+};
 
-  const tip1 = bill * (percentage1 / 100);
-  const tip2 = bill * (percentage2 / 100);
-  const tip = (tip1 + tip2) / 2;
+const Logo = () => {
+  return (
+    <div className="logo">
+      <span role="img">🍿</span>
+      <h1>usePopcorn</h1>
+    </div>
+  );
+};
 
-  const setReset = () => {
-    setBill("");
-    setPercentage1(0);
-    setPercentage2(0);
-  };
+const Search = () => {
+  const [query, setQuery] = useState("");
 
   return (
-    <div>
-      <BillInput bill={bill} setBill={setBill} />
+    <input
+      className="search"
+      type="text"
+      placeholder="Search movies..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+    />
+  );
+};
 
-      <Select percentage={percentage1} setPercentage={setPercentage1}>
-        How much was the bill?
-      </Select>
+const Main = () => {
+  return (
+    <main className="main">
+      <MovieList />
+      <WatchedList />
+    </main>
+  );
+};
 
-      <Select percentage={percentage2} setPercentage={setPercentage2}>
-        How did your friend like the service?
-      </Select>
+const MovieList = () => {
+  const [movies, setMovies] = useState(tempMovieData);
+  const [isOpen1, setIsOpen1] = useState(true);
 
-      <div style={{ marginTop: "20px" }}>
-        {bill > 0 && <Output bill={bill} tip={tip} setReset={setReset} />}
+  return (
+    <div className="box">
+      <button
+        className="btn-toggle"
+        onClick={() => setIsOpen1((open) => !open)}
+      >
+        {isOpen1 ? "–" : "+"}
+      </button>
+      {isOpen1 && (
+        <ul className="list">
+          {movies?.map((movie) => (
+            <Movie movie={movie} key={movie.imdbID} />
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+const Movie = ({ movie }) => {
+  return (
+    <li key={movie.imdbID}>
+      <img src={movie.Poster} alt={`${movie.Title} poster`} />
+      <h3>{movie.Title}</h3>
+      <div>
+        <p>
+          <span>🗓</span>
+          <span>{movie.Year}</span>
+        </p>
+      </div>
+    </li>
+  );
+};
+
+const WatchedList = () => {
+  const [watched, setWatched] = useState(tempWatchedData);
+  const [isOpen2, setIsOpen2] = useState(true);
+
+  return (
+    <div className="box">
+      <button
+        className="btn-toggle"
+        onClick={() => setIsOpen2((open) => !open)}
+      >
+        {isOpen2 ? "–" : "+"}
+      </button>
+      {isOpen2 && (
+        <>
+          <WatchedSummery watched={watched} />
+          <WatchedMovieList watched={watched} />
+        </>
+      )}
+    </div>
+  );
+};
+
+const WatchedSummery = ({ watched }) => {
+  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
+  const avgUserRating = average(watched.map((movie) => movie.userRating));
+  const avgRuntime = average(watched.map((movie) => movie.runtime));
+
+  return (
+    <div className="summary">
+      <h2>Movies you watched</h2>
+      <div>
+        <p>
+          <span>#️⃣</span>
+          <span>{watched.length} movies</span>
+        </p>
+        <p>
+          <span>⭐️</span>
+          <span>{avgImdbRating}</span>
+        </p>
+        <p>
+          <span>🌟</span>
+          <span>{avgUserRating}</span>
+        </p>
+        <p>
+          <span>⏳</span>
+          <span>{avgRuntime} min</span>
+        </p>
       </div>
     </div>
   );
 };
 
-const Select = ({ children, percentage, setPercentage }) => {
+const WatchedMovieList = ({ watched }) => {
   return (
-    <div>
-      <label>{children}</label>
-      <select
-        value={percentage}
-        onChange={(e) => setPercentage(e.target.value)}
-      >
-        <option value="0">Dissatisfied (0%)</option>
-        <option value="5">It was okay (5%)</option>
-        <option value="10">It was good (10%)</option>
-        <option value="20">Absolutely amazing! (20%)</option>
-      </select>{" "}
-    </div>
+    <ul className="list">
+      {watched.map((movie) => (
+        <WatchedMovie movie={movie} key={movie.imdbID}/>
+      ))}
+    </ul>
   );
 };
 
-function BillInput({ bill, setBill }) {
+const WatchedMovie = ({ movie }) => {
   return (
-    <div>
-      <span>How much the bill ? : </span>
-      <span>
-        <input
-          type="text"
-          placeholder="Bill Value"
-          value={bill}
-          onChange={(e) => setBill(e.target.value)}
-        />
-      </span>
-    </div>
+    <li key={movie.imdbID}>
+      <img src={movie.Poster} alt={`${movie.Title} poster`} />
+      <h3>{movie.Title}</h3>
+      <div>
+        <p>
+          <span>⭐️</span>
+          <span>{movie.imdbRating}</span>
+        </p>
+        <p>
+          <span>🌟</span>
+          <span>{movie.userRating}</span>
+        </p>
+        <p>
+          <span>⏳</span>
+          <span>{movie.runtime} min</span>
+        </p>
+      </div>
+    </li>
   );
-}
-
-function Output({ bill, tip, setReset }) {
-  return (
-    <>
-      <h3>
-        You pay {parseFloat(bill) + parseFloat(tip)} ({bill} + {tip} tip)
-      </h3>
-      <Reset setReset={setReset} />
-    </>
-  );
-}
-
-function Reset({ setReset }) {
-  return <button onClick={setReset}>Reset</button>;
-}
-
-export default App;
+};
--- src/components/Logo.jsx
+++
@@ -1,6 +0,0 @@
-
-const Logo = () => {
-   return <h1>Far Away</h1>;
-}
-
-export default Logo
